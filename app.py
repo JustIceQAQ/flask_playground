ae9198ee00589e5bd187745e53a5ce68c3d2d20e
JustIceQAQ <justiceqaq@gmail.com>
2022-02-09 16:12:48 +0800

    add: video app route

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@app.route("/video", methods=['GET', 'POST'])
+def video():
+    info = "<p>{}</p>"
+    if request.method == 'POST':
+        info = info.format("download video?")
+    elif request.method == 'GET':
+        info = info.format("upload video?")
+    return info
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)

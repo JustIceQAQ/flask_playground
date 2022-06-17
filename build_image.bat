44babc7e9f5bdafc4f9669f33ef0129797b2c9fc
justiceqaq <justiceqaq@gmail.com>
2022-06-18 03:23:34 +0800

    add: build_image.bat (windows)

---
+++ build_image.bat
@@ -0,0 +1,13 @@
+@echo off
+SETLOCAL
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+set "fullTimeStamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
+for /f "delims== tokens=1,2" %%G in (.env) do set %%G=%%H
+
+%dockerCli% build -t flask_playground:%fullTimeStamp% .
+
+ENDLOCAL
